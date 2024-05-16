ce3dfdd4f5eb7a491c8503fa2f56ef063d758e1d
Krishna Sahu <dvlprkrishna@gmail.com>
2024-05-16 15:33:44 +0530

    Test

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -83,7 +83,6 @@ const DesktopNavbar = (props: any) => {
           return;
         }
       }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
 
@@ -250,30 +249,29 @@ const DesktopNavbar = (props: any) => {
     </motion.div>
   );
 };
-const ListItem: React.FC<{
-  className?: string;
-  title: string;
-  href: string;
-  children: React.ReactNode;
-}> = ({ className, title, href, children }) => {
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
+        <a
+          ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
-          href={href}
+          {...props}
         >
           <div className="text-sm font-medium leading-none">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   );
-};
+});
 ListItem.displayName = "ListItem";
 export default DesktopNavbar;
